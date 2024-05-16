bf1018fbc31bf12811c1e33aef66d1382b902ee9
太陽 岡山 <220069@jc-21.jp>
2024-05-16 16:30:06 +0900

    GameSelectSceneクラス:コード書き中

--- Scene/GameSelectScene.cpp
+++ Scene/GameSelectScene.cpp
@@ -1,1 +1,36 @@
+//インクルード
+#include "../Engine/Input.h"
+#include "../Engine/SceneManager.h"
+#include "../Engine/Direct3D.h"
+#include "../Engine/Image.h"
+#include "../Engine/Global.h"
+#include "../StageObject/StageObjectManager.h"
+#include "../StageObject/SolidText.h"
+#include "../StageObject/Sky.h"
+#include "../Player/AttackPlayer.h"
+#include "../Player/CollectPlayer.h"
+#include "../Player/ActorAttackPlayer.h"
+#include "../Player/ActorCollectPlayer.h"
 #include "GameSelectScene.h"
+
+GameSelectScene::GameSelectScene(GameObject* _pParent)
+	:GameObject(_pParent,gameSelectSceneName), hPict_{-1},attackOrCollect_{0},attackOrCollectInverse_{0},padIDNum_{0}
+	,skyPos_{0.0f,0.0f,0.0f},skyPosFly_{10000}
+{
+}
+
+void GameSelectScene::Initialize()
+{
+}
+
+void GameSelectScene::Update()
+{
+}
+
+void GameSelectScene::Draw()
+{
+}
+
+void GameSelectScene::Release()
+{
+}
--- Scene/GameSelectScene.h
+++ Scene/GameSelectScene.h
@@ -3,8 +3,36 @@
 #include "../Engine/GameObject.h"
 #include "../Engine/Camera.h"
 
-
-class GameSelectScene
+namespace
 {
+	std::string gameSelectSceneName = "GameSelectScene";
+}
+
+class SceneManager;
+class SolidText;
+class Sky;
+class ActorAttackPlayer;
+class ActorCollectPlayer;
+
+class GameSelectScene : public GameObject
+{
+private:
+	int hPict_;
+	int attackOrCollect_;
+	int attackOrCollectInverse_;
+	bool padIDNum_;
+	XMFLOAT3 skyPos_;
+	XMFLOAT3 skyPosFly_;
+	XMFLOAT3 camPos_;
+	SceneManager* pSceneManager_;
+	StageObjectManager* pStageObjectManager_;
+	SolidText* pSolidText_;
+	Sky* pSky_;
+public:
+	GameSelectScene(GameObject* _pParent);
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Release() override;
 };
 
